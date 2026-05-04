b85479716a5c04e95443f212253b7aec0636d5e3
NightRaccoon511 <74578591+NightRaccoon511@users.noreply.github.com>
2026-05-04 13:51:03 -0400

    Initialize ContactBook with sample contacts

--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -4,10 +4,13 @@ public class Program
 {
     public static void Main()
     {
-        var cb = new ContactBook();
+        var c1 = new Contact("Henry", "Bruckman", "123-456-7890", "hbruckman@gmail.com");
+        var c2 = new Contact("Alice", "Smith", "987-654-3210", "asmith@gmail.com");
+        var cb = new ContactBook(new List<Contact> { c1, c2 });
         cb.Start();
     
 
     }
 }
+
 
